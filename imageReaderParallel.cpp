9f68382482649b016f5ec84c467b58c5b8286ebf
Luca0079 <luca.leuter@stud.unifi.it>
2022-11-26 17:54:54 +0100

    fix parallel version and add test in main file

--- imageReaderParallel.cpp
+++ imageReaderParallel.cpp
@@ -1,42 +1,20 @@
 #include "imageReaderParallel.h"
-
 Mat* images1;
 
 void loadImagesParallel(std::vector<std::string> imageStrings){
-    int nthreads, tid;
+    int tid;
     tid = -1;
 
-#ifdef _OPENMP
-    std::cout << "_OPENMP defined" << std::endl;
-    std::cout << "Num processors (Phys+HT): " << omp_get_num_procs() << std::endl;
-#endif
-
-    printf("Global copy of tid before threads execution = %d\n\n", tid);
-
-/* Fork a team of threads giving them their own copies of variables */
-#pragma omp parallel private(nthreads, tid)
+#pragma omp parallel private(tid)
     {
-
 #ifdef _OPENMP
         /* Obtain thread number */
         tid = omp_get_thread_num();
         printf("Hello World from thread = %d\n", tid);
-
-        /* Only master thread does this */
-        if (tid == 0) {
-            nthreads = omp_get_num_threads();
-            printf("Master thread - Number of threads = %d\n", nthreads);
-        }
 #endif
-
-        tid += 100;
-        printf("Local copy of tid = %d\n", tid);
-
     }
-    /* All threads join master thread and are destroyed */
-    printf("\nGlobal copy of tid after threads execution = %d\n", tid);
 
-    // Parallel loading of images2
+    // Parallel loading of images1
     images1 = new Mat[imageStrings.size()];
 #pragma omp parallel for
     for(int i=0; i<imageStrings.size(); i++)
--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <opencv2/opencv.hpp>
 #include <chrono>
 #include <filesystem>
+#include <fstream>
 
 #include "imageReaderSequential.h"
 #include "imageReaderParallel.h"
+
+#define nTest 1
 
 using namespace cv;
 using namespace std;
@@ -12,15 +15,15 @@ namespace fs = std::filesystem;
 
 int main() {
     String imagePath = "/home/luca/CLionProjects/Image_Reader/images/";
+    ofstream logFile("log.txt");
     std::vector<std::string> imageStrings = std::vector<std::string>();
+    int maxThreads = omp_get_max_threads();
     // Trova i nomi delle immagini all'interno della cartella
     for (auto &p: std::filesystem::directory_iterator(imagePath))
         imageStrings.emplace_back(p.path().string());
 
     Mat *images;
-    int nTest = 2;
     int sequentialTime = 0;
-    int parallelTime = 0;
 
     // Caricamento sequenziale delle immagini
     for (int i = 0; i < nTest; i++) {
@@ -31,20 +34,29 @@ int main() {
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
         sequentialTime += elapsed.count();
     }
-    cout << "Tempo medio trascorso per il caricamento delle immagini in modo sequenziale: "<<sequentialTime/nTest<<"\n";
+    cout << "Tempo medio trascorso per il caricamento delle immagini in modo sequenziale: "
+         << sequentialTime / nTest << "\n";
+    logFile << sequentialTime / nTest << "\n";
 
-    // Caricamento parallelo delle immagini
-    for (int i = 0; i < nTest; i++) {
-        auto start = std::chrono::system_clock::now();
-        loadImagesParallel(imageStrings);
-        images = getImagesSequential();
-        auto end = std::chrono::system_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        parallelTime += elapsed.count();
+    for(int k = 2; k < (maxThreads + 1); k++) {
+        int parallelTime = 0;
+        // Cambia il numero di thread
+        omp_set_num_threads(k);
+
+        // Caricamento parallelo delle immagini
+        for (int i = 0; i < nTest; i++) {
+            auto start = std::chrono::system_clock::now();
+            loadImagesParallel(imageStrings);
+            images = getImagesSequential();
+            auto end = std::chrono::system_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+            parallelTime += elapsed.count();
+        }
+        cout << "Tempo medio trascorso per il caricamento delle immagini in modo parallelo: "
+            << parallelTime / nTest << "\n";
+        logFile << parallelTime / nTest << "\n";
     }
-    cout << "Tempo medio trascorso per il caricamento delle immagini in modo parallelo: " << parallelTime / nTest;
-
-
+    logFile.close();
     // Test per visualizzare immagini
     /*
     String windowName = "Display window";
